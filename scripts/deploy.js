93d34434ecaf220267822e01f6c7e42926424861
何绪杰 <hexujie@oneship.cc>
2018-12-12 10:58:54 +0800

    deploy时 打印输出带color的文字

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,21 @@
-var ghpages = require('gh-pages');
-var path = require('path');
+require('colors-cli/toxic')
+const ghpages = require('gh-pages')
+const path = require('path')
+
 const dirName = path.resolve() + '/build'
 
-ghpages.publish(dirName,
-  {
-    branch: 'master',
-    repo: 'git@github.com:Jackson-he/Jackson-he.github.io.git'
-  },
-  function(err) {
-    if (err) {
-      console.log(err)
-    } else {
-      console.log('deploy succeed!!!')
-    }
+const config = {
+  branch: 'master',
+  repo: 'git@github.com:Jackson-he/Jackson-he.github.io.git'
+}
+
+
+const cb = function(err) {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(`${'deploy succeed ^_^'.green}\nyour application has been pushed to the repository: ${config.repo.cyan_bt}\nbased on branch: ${config.branch.cyan_bt}`)
   }
-);
+}
+
+ghpages.publish(dirName, config, cb);
